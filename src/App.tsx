3db710511703c6b78477a2aff513b85d9496ea1f
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-07-14 18:51:13 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,137 +1,137 @@
-import { Dropdown, IDropdownStyles } from "@fluentui/react";
+import { Joystick } from "lingo3d-react"
 import {
   Dummy,
   Model,
   Reflector,
-  Setup,
   ThirdPersonCamera,
   World,
   types,
   usePreload,
-  useWindowSize,
   keyboard,
-} from "lingo3d-react";
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
+  TweakPane,
+  PaneInput
+} from "lingo3d-react"
+import { useEffect, useRef, useState } from "react"
+import "./App.css"
 
+// Game world component
+// 场景组件
 const Game = () => {
-  const dummyRef = useRef<types.Dummy>(null);
-  const { width } = useWindowSize();
-  const [strideMode, setStrideMode] = useState<"aim" | "free">("aim");
-  const [lockTargetRotation, setlockTargetRotation] = useState<
-    false | "lock" | "dynamic-lock"
-  >("lock");
+  // ref to player character model
+  // 玩家角色模型的ref
+  const dummyRef = useRef<types.Dummy>(null)
 
-  const dropdownStyles: Partial<IDropdownStyles> = {
-    dropdown: { width: 300 },
-  };
+  // If strideMode is "aim", camera is always behind character when character is moving
+  // 如果strideMode为"aim", 当角色移动时, 摄像机始终在角色身后
+  const [strideMode, setStrideMode] = useState<"aim" | "free">("aim")
 
+  // If lockTargetRotation is "true", character always faces the direction that the camera looks at
+  // 如果lockTargetRotation为"true", 角色始终面向摄像机的朝向
+  const [lockTargetRotation, setlockTargetRotation] = useState<boolean | "dynamic-lock">(true)
+
+  // listen to keyboard events, enable WASD controls
+  // 监听键盘事件，启用WASD控制
   useEffect(() => {
-    const dummy = dummyRef.current;
-    if (!dummy) return;
+    const dummy = dummyRef.current
+    if (!dummy) return
 
     keyboard.onKeyPress = (_, keys) => {
-      if (keys.has("w")) dummy.strideForward = -10;
-      else if (keys.has("s")) dummy.strideForward = 10;
-      else dummy.strideForward = 0;
+      // stride forward and stride right determine the direction the player is moving towards
+      // 向前和向右的步伐，用于确定玩家的移动方向
+      if (keys.has("w")) dummy.strideForward = -10
+      else if (keys.has("s")) dummy.strideForward = 10
+      else dummy.strideForward = 0
 
-      if (keys.has("a")) dummy.strideRight = 10;
-      else if (keys.has("d")) dummy.strideRight = -10;
-      else dummy.strideRight = 0;
-    };
-  }, []);
+      if (keys.has("a")) dummy.strideRight = 10
+      else if (keys.has("d")) dummy.strideRight = -10
+      else dummy.strideRight = 0
+    }
+  }, [])
 
   return (
-    <>
-      <World>
-        <Model
-          physics="map"
-          bloom
-          width={245.36}
-          depth={245.36}
-          scaleX={20}
-          scaleY={20}
-          scaleZ={20}
-          src="scene.glb"
+    <World
+      defaultFog="white"
+      defaultLight="env.jpg"
+      bloomStrength={1}
+      bloomThreshold={0.5}
+    >
+      {/* map model, bloom enabled */}
+      {/* 地图模型, 启用泛光特效 */}
+      <Model physics="map" bloom scale={20} src="scene.glb" />
+
+      {/* character model, and the camera that follows it */}
+      {/* 角色模型，和跟随它的相机 */}
+      <ThirdPersonCamera
+        active
+        mouseControl="drag"
+        lockTargetRotation={lockTargetRotation}
+        enableDamping
+      >
+        <Dummy
+          physics="character"
+          ref={dummyRef}
+          metalnessFactor={-0.5}
+          roughnessFactor={0}
+          y={44.58}
+          strideMove
+          strideMode={strideMode}
         />
-        <ThirdPersonCamera
-          active
-          mouseControl="drag"
-          fov={width < 640 ? 110 : 90}
-          lockTargetRotation={lockTargetRotation}
-        >
-          <Dummy
-            physics="character"
-            ref={dummyRef}
-            metalnessFactor={0.5}
-            roughnessFactor={0.5}
-            bloom
-            y={44.58}
-            strideMove
-            strideMode={strideMode}
-          />
-        </ThirdPersonCamera>
-        <Reflector
-          y={-39.38}
-          scaleX={113.2}
-          scaleY={113.2}
-          rotationX={-90}
-          contrast={1}
+      </ThirdPersonCamera>
+
+      {/* Ground reflection */}
+      {/* 地面反射 */}
+      <Reflector y={-39.38} scale={113.2} />
+
+      {/* joystick */}
+      {/* 摇杆 */}
+      <Joystick
+        onMove={(e) => {
+          const dummy = dummyRef.current
+          if (!dummy) return
+
+          dummy.strideForward = -e.y * 5
+          dummy.strideRight = -e.x * 5
+        }}
+        onMoveEnd={() => {
+          const dummy = dummyRef.current
+          if (!dummy) return
+
+          dummy.strideForward = 0
+          dummy.strideRight = 0
+        }}
+      />
+
+      {/* Options panel */}
+      {/* 参数面板 */}
+      <TweakPane>
+        <PaneInput
+          name="strideMode"
+          value="aim"
+          values={["aim", "free"]}
+          onChange={(val) => setStrideMode(val)}
         />
-        <Setup
-          defaultFog="white"
-          defaultLight="env1.jpg"
-          bloomStrength={1}
-          bloomThreshold={0.5}
+        <PaneInput
+          name="lockTargetRotation"
+          value={true}
+          values={[true, false, "dynamic-lock"]}
+          onChange={(val) => setlockTargetRotation(val)}
         />
-      </World>
-      <div className="absolute  backdrop-blur-xl -mt-10 px-4 top-10">
-        <div className={"h-2"} />
-        <Dropdown
-          placeholder="Select an option"
-          label="Camera lockTargetRotation 相机锁定目标旋转"
-          options={[
-            { key: "false", text: "false 不锁定" },
-            { key: "lock", text: "lock 锁定" },
-            { key: "dynamic-lock", text: "dynamic-lock 动态锁定" },
-          ]}
-          styles={dropdownStyles}
-          onChange={(_, option) => {
-            if (!option) return;
-            if (option.key === "false") setlockTargetRotation(false);
-            else setlockTargetRotation(option.key as any);
-          }}
-        />
-        <div className={"h-4"} />
-        <Dropdown
-          placeholder="Select an option"
-          label="Dummy strideMode 角色步伐模式"
-          options={[
-            { key: "aim", text: "aim 瞄准模式" },
-            { key: "free", text: "free 自由模式" },
-          ]}
-          styles={dropdownStyles}
-          onChange={(_, option) => {
-            if (!option) return;
-            setStrideMode(option.key as any);
-          }}
-        />
-      </div>
-    </>
-  );
-};
+      </TweakPane>
+    </World>
+  )
+}
 
 const App = () => {
-  const progress = usePreload(["scene.glb"], "1.2mb");
+  const progress = usePreload(["scene.glb"], "1.2mb")
 
   if (progress < 100)
     return (
       <div className="w-screen h-screen absolute left-0 top-0 text-white bg-black flex items-center justify-center">
         loaded: {Math.round(progress)}%
       </div>
-    );
+    )
 
-  return <Game />;
-};
+  return <Game />
+}
 
-export default App;
+export default App
